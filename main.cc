4c3baeae62d800eafa186be00af9dda74cf962a3
ValentinAvram <p92avavv@uco.es>
2020-12-05 17:49:31 +0100

    Primera actualizacion main.cc

--- main.cc
+++ main.cc
@@ -4,5 +4,8 @@
 #include <senderos.h>
 
 void main(){
-  cin<<"veamos si funciona correctamente";
+  cin<<"--- Bienvenido al Sistema de Gestión de Parques Naturales de la Junta de Andalucia. ---\n";
+  cin<<"--- Por favor, inicie sesión. ---\n";
+  cin<<"--- 0) Entrar al Sistema como Administrador. ---\n";
+  cin<<"--- 1) Entrar al Sistema como Monitor. ---\n";
 }
