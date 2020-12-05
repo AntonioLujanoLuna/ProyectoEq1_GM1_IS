199967c88848e7bab837d3b582ae632a70b6d5f6
ValentinAvram <p92avavv@uco.es>
2020-12-05 19:53:24 +0100

    7º actualizacion main.cc

--- main.cc
+++ main.cc
@@ -11,9 +11,14 @@
 using namespace std;
 
 int main(){
+
+  menuPrincipal();
+}
+
+int menuPrincipal(){
   int login;
 
-  cout<<"--- Bienvenido al Sistema de Gestión de Parques Naturales de la Junta de Andalucia. ---\n";
+  cout<<"     --- Bienvenido al Sistema de Gestión de Parques Naturales de la Junta de Andalucia. ---\n";
   cout<<"--- Por favor, inicie sesion, seleccionado la opcion 0 o 1. ---\n";
   cout<<"--- 0) Entrar al Sistema como Administrador. ---\n";
   cout<<"--- 1) Entrar al Sistema como Monitor. ---\n";
@@ -21,27 +26,50 @@ int main(){
   cin<<login;
 
   switch(login){
+    while(login<0||login>3){
+      cout<<"Opcion invalida. Por favor, seleccione una opcion valida. \n";
+      cin<<login;
+    }
 
   case 0:
+    if(comprobarAdmin==false){
+      cout<<"No tiene autorizacion para entrar al sistema como administrador. \n";
+    }
+    else{
+      menuAdmin();
+    }
+
+  case 1:
+  if(comprobarMonitor==false){
+    cout<<"No tiene autorizacion para entrar al sistema como monitor. \n";
+  }
+  else{
+    menuMonitor();
+  }
+
+  case 2:
+  return 0;
+  break;
+
+  }
+}
+
+int menuAdmin(){
     int opc;
-    string password;
-      cout<<" Por favor, introduzca su contraseña.\n";
-      cin>>password;
-      while(password!=admingestionJA){
-        cout<<" Contraseña incorrecta. Por favor, introduzca su contraseña.\n";
-        cin>>password;
-      }
-      cout<<" Contraseña correcta. Bienvenido al sistema. \n";
-      cout<<" Por favor, seleccione a que gestion desea acceder. \n";
+      cout<<"--- Contraseña correcta. Bienvenido al sistema. ---\n";
+      cout<<"--- Por favor, seleccione a que gestion desea acceder. ---\n";
       cout<<"--- 0) Gestion de Parques Naturales. ---\n";
       cout<<"--- 1) Gestion de Senderos. ---\n";
       cout<<"--- 2) Gestion de Visitantes. ---\n";
       cout<<"--- 3) Gestion de Monitores. ---\n";
+      cout<<"--- 4) Salir del menu. ---\n";
       cin<<opc;
+
       while(opc<0||opc>4){
         cout<<" Opcion invalida. Por favor, seleccione una opcion valida. \n";
         cin<<opc;
       }
+
       case 0:
         menuParques();
         break;
@@ -59,35 +87,106 @@ int main(){
         break;
 
       case 4:
-        return 0;
+        menuPrincipal();
         break;
+}
 
-    break;
-
-  case 1:
+int menuMonitor(){
   int seleccion;
+    cout<<"--- DNI correcto. Bienvenido al sistema. ---\n";
+    cout<<"--- Por favor, seleccione a que gestion desea acceder. ---\n";
+    cout<<"--- 0) Gestion de Rutas. ---\n";
+    cout<<"--- 1) Salir del menu ---\n";
+    cin<<seleccion;
+
+    switch(seleccion){
+      while(seleccion<0||seleccion>1){
+        cout<<"Opcion invalida. Por favor seleccione una opcion valida \n";
+        cin<<seleccion;
+      }
+
+    case 0:
+      menuRutas();
+      break;
+
+    case 1:
+      menuPrincipal;
+      break;
+  }
+}
+
+bool comprobarAdmin(bool es_Admin){
+  string password;
+  es_Admin=false;
+    cout<<" Por favor, introduzca su contraseña.\n";
+    cin>>password;
+    while(password!=admingestionJA){
+      cout<<" Contraseña incorrecta. Por favor, introduzca su contraseña.\n";
+      cin>>password;
+    }
+es_Admin=true;
+return es_Admin;
+}
+
+bool comprobarMonitor(bool es_Monitor){
+  es_Monitor=false;
   string dni;
     cout<<" Por favor, introduzca su DNI.\n";
     cin>>DNI;
     //Funcion comprobacion DNI
     //
     //
-    cout<<" DNI correcto. Bienvenido al sistema. \n";
-    cout<<" Por favor, seleccione a que gestion desea acceder. \n";
-    cout<<"--- 0) Gestion de Rutas. ---\n";
-    cout<<"--- 1) Salir del programa ---\n";
-    case 0:
-    menuRutas();
-    break;
+return es_Monitor;
+}
 
-    case 1:
-      return 0;
-      break;
+int menuParques(){
+  int i;
+  cout<<"   --- Gestion de Parques --- \n";
+  cout<<"--- Seleccione la opcion a la que desee acceder. --- \n";
+  cout<<"--- 0) Añadir un nuevo Parque Natural. --- \n";
+  cout<<"--- 1) Modificar los datos de un Parque Natural ya introducido. --- \n";
+  cout<<"--- 2) Cambiar la disponibilidad de un Parque Natural. --- \n";
+  cout<<"--- 3) Eliminar un Parque Natural. --- \n";
+  cout<<"--- 4) Mostrar los datos de un Parque Natural ya introducido. --- \n";
+  cout<<"--- 5) Salir del programa. --- \n";
+  cin<<i;
 
+  while(i<0||i>5){
+    cout<<" Opcion invalida. Por favor, seleccione una opcion valida.\n";
+    cin<<i;
+  }
+
+  case 0:
+  //
+  //
+  //
+  break;
+
+  case 1:
+  //
+  //
+  //
   break;
 
   case 2:
+  //
+  //
+  //
+  break;
+
+  case 3:
+  //
+  //
+  //
+  break;
+
+  case 4:
+  //
+  //
+  //
+  break;
+
+  case 5:
     return 0;
-    break;
-  }
+  break;
 }
