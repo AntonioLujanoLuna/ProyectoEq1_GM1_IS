c9de69ac53a27980fe90e5f1c201a4048465a259
ValentinAvram <p92avavv@uco.es>
2020-12-05 18:07:21 +0100

    Segunda actualizacion main.cc

--- main.cc
+++ main.cc
@@ -2,10 +2,29 @@
 //proyecto IS
 #include <parques.h>
 #include <senderos.h>
+using namespace std;
 
 void main(){
-  cin<<"--- Bienvenido al Sistema de Gestión de Parques Naturales de la Junta de Andalucia. ---\n";
-  cin<<"--- Por favor, inicie sesión. ---\n";
-  cin<<"--- 0) Entrar al Sistema como Administrador. ---\n";
-  cin<<"--- 1) Entrar al Sistema como Monitor. ---\n";
+  int login;
+  char password;
+  cout<<"--- Bienvenido al Sistema de Gestión de Parques Naturales de la Junta de Andalucia. ---\n";
+  cout<<"--- Por favor, inicie sesión, seleccionado la opción 0 o 1. ---\n";
+  cout<<"--- 0) Entrar al Sistema como Administrador. ---\n";
+  cout<<"--- 1) Entrar al Sistema como Monitor. ---\n";
+  cin<<login;
+
+  switch(login){
+
+    case 0:
+    do{
+      cout<<" Por favor, introduzca su contraseña.\n";
+      cin>>password;
+      while(password!=admingestionJA){
+        cout<<" Contraseña incorrecta. Por favor, introduzca su contraseña.\n"
+        cin>>password;
+      }
+      cout<<" Contraseña correcta. Bienvenido al sistema. \n"
+      cout<<" Por favor, selecciona a "
+    }
+  }
 }
